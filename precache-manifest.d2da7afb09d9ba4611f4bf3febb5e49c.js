678e46b440eecb64fbdaa37c290197bcb5566e1b
justix <justix36@gmail.com>
2022-09-08 19:10:10 +0300

    Updates

---
+++ precache-manifest.d2da7afb09d9ba4611f4bf3febb5e49c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "48422ee4b2acbce0b1b7",
+    "url": "/react-food/static/css/main.dfb1898d.chunk.css"
+  },
+  {
+    "revision": "48422ee4b2acbce0b1b7",
+    "url": "/react-food/static/js/main.48422ee4.chunk.js"
+  },
+  {
+    "revision": "1860cc2f2f785f13a111",
+    "url": "/react-food/static/js/1.1860cc2f.chunk.js"
+  },
+  {
+    "revision": "320bfd5917fc4f3c7449",
+    "url": "/react-food/static/js/runtime~main.320bfd59.js"
+  },
+  {
+    "revision": "a559a47f6a17e05c35ab371941878916",
+    "url": "/react-food/index.html"
+  }
+];
